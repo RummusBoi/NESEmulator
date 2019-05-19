930b88f6ab72c0bca8603a453a18b25e897b68bf
RummusBoi <rasmussamsing@hotmail.dk>
2019-05-19 17:44:14 +0200

    Added helper functions to instruction.py. Added lambda functions to acces memory in constant time.

--- Instruction.py
+++ Instruction.py
@@ -11,13 +11,41 @@ class Instruction(ABC):
     def execute(self, opcode: bytearray, p: bytearray, ram: RAM):
         pass
 
-def getMemoryLocation(opcode: bytearray)
+#lambda functions for getting value from memory, sorted by (d,x : d,y : a,x : a,y : (d,x) : (d),y)
+getMemArray = [
+    lambda cpu, nextbytes: (nextbytes[0][0] + cpu.X) % 256,
+    lambda cpu, nextbytes: (nextbytes[0][0] + cpu.Y) % 256,
+    lambda cpu, nextbytes: (nextbytes[0][0] + cpu.X),
+    lambda cpu, nextbytes: (nextbytes[0][0] + cpu.Y),
+    lambda cpu, nextbytes: (cpu.ram.read_bytes(getMemArray[0](cpu, nextbytes), 1) + cpu.ram.read_bytes((nextbytes[0][0] + cpu.X + 1) % 256, 1)) * 256,
+    lambda cpu, nextbytes: (cpu.ram.read_bytes(nextbytes[0][0], 1) + cpu.ram.read_bytes((nextbytes[0][0] + 1) % 256, 1) * 256 + cpu.Y)
+    #TODO reorder these such they fit the correct opcodes.
+    #TODO add the remaining addressing modes, might not need (d,y) mode for ALU.
+    ]
 
-class ADC(Instruction):
+def ALUMemHelper(f, opcode: bytearray, cpu: CPU.CPU):
+    #find the addressing mode of this operation. The addressing mode for the ALU operations are stored in bit 2, 3 and 4.
+    code = opcode & 11100
+
+    #use the helper function to retrieve the byte from memory
+
+    nextbytes = cpu.ram.read_bytes(cpu.PC, 2) #TODO assumes the program counter has already been incremented
+    val = cpu.ram.read_bytes(getMemArray[code](cpu, nextbytes), 1)
+
+    #perform the ALU operation on the retrieved byte
+    result = f(cpu.A, val)
+
+    #save result to accumulator
+    cpu.A += result    
+
+    #TODO where should processor status be updated?
+    #TODO where should
+
+
+#ALU instructions. Instructions whose least significant bits are (01)
+
+class ORA(Instruction):
     def __init__(self):
         self.value = 2
-    
-    def execute(self, opcode: bytearray, cpu: CPU.CPU):
-        if(opcode == 0x69):
-            
-            
+    def execute(self, opcode: bytearray, p: bytearray, ram: RAM, pc):
+        ALUMemHelper (lambda a, b: a | b)
